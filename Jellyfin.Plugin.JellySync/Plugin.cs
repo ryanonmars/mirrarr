91d6fb215d8ccd09d7826f01711c0bab1cd02d37
ryanonmars <169190325+ryanonmars@users.noreply.github.com>
2026-08-29 22:50:54 -0400

    Bypass stale JellySync dashboard module cache

--- Jellyfin.Plugin.JellySync/Plugin.cs
+++ Jellyfin.Plugin.JellySync/Plugin.cs
@@ -31,7 +31,7 @@ public sealed class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 
         yield return new PluginPageInfo
         {
-            Name = "jellysync.js",
+            Name = "jellysync-v0.1.6.js",
             EmbeddedResourcePath = $"{GetType().Namespace}.Configuration.configPage.js",
         };
     }
